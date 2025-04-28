73e9924fd40fb1100f987ce602da98876dc56dfe
Siddhant Madhur <siddhant.madhur@gmail.com>
2025-04-28 14:11:33 -0700

    finished auth

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,16 +1,51 @@
 import { Button, StyleSheet, Text, View } from "react-native";
 import { signInWithGoogle} from "../lib/auth"
-import { useEffect } from "react";
-import auth from "@react-native-firebase/auth"
+import { useEffect, useState } from "react";
+import auth, { FirebaseAuthTypes } from "@react-native-firebase/auth"
 
 export default function Index() {
 
+    const [user, setUser] = useState<null | FirebaseAuthTypes.User>(null)
+    const [initializing, setInitializing] = useState(true)
+
     useEffect(()=>{
         const subscriber = auth().onAuthStateChanged((user)=>{
-            console.log("Detected change: ", user) 
+            if (user) {
+                setUser(user)
+            }
+            if (initializing) setInitializing(false)
         })
         return subscriber;
     },[])
+
+
+    if (initializing) {
+        return null;
+    }
+    if (user) {
+        return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "space-between",
+        alignItems: "center",
+        gap: 5,
+        alignSelf: 'stretch',
+        paddingBlock: 30,
+      }}
+    >
+      <Text 
+        style={{
+            fontSize: 24,
+            fontWeight: 'bold',
+        }}
+      >
+      Welcome to RideShare: {user.displayName}
+      </Text>
+    </View>
+
+        )
+    }
 
   return (
     <View
